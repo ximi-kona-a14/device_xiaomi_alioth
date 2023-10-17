6a2a6d660d4008b06aec4750e63392c57b1c042c
Vertigo <vertigo.builds38@gmail.com>
2023-10-17 20:47:58 +0530

    alioth: init POSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/potato_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    potato_alioth-user 
